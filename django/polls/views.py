bae1011535d7525fa3fdb3145ca984e93b2567bb
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2024-08-22 17:34:04 +0900

    詳細ページ

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Question
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -17,7 +17,14 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    # 以下と等価
+    # from django.http import Http404
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
