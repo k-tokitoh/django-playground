4a4236f6ef7abae30ab001cf5aaf5dfd4cddda65
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2024-08-22 16:26:21 +0900

    add template

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # {application}/templates の中を探してくれる
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
+    # render()というショートカットは以下と等価
+    # from django.template import loader
+    # template = loader.get_template("polls/index.html")
+    # return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
