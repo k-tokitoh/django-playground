e1dd1ab05c992fb5bbf01930f00965314801175d
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2024-08-26 15:11:21 +0900

    productionで環境変数によりDB接続

--- awesome-django-project/settings/production.py
+++ awesome-django-project/settings/production.py
@@ -1,1 +1,15 @@
+import os
+
 from .base import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        # docker-compose.ymlでの設定と一致させる
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+    }
+}
