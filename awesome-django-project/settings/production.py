c360c12c366816bd120d962d36ae76a3d46c2bb5
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2024-08-29 19:09:47 +0900

    static deploy周り、色々試したのを整理

--- awesome-django-project/settings/production.py
+++ awesome-django-project/settings/production.py
@@ -26,8 +26,6 @@ STORAGES = {
             "bucket_name": "djangoplayground-dev-static-rbctek",
             "file_overwrite": False,
             "location": "production",
-            "access_key": os.getenv("AWS_ACCESS_KEY_ID"),
-            "secret_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
         },
     }
 }
