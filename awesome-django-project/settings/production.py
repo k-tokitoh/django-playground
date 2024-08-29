3bbe169a7b7b3ef71ed52d25f3248d3b39b7f000
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2024-08-29 22:03:14 +0900

    logging

--- awesome-django-project/settings/production.py
+++ awesome-django-project/settings/production.py
@@ -29,3 +29,24 @@ STORAGES = {
         },
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
